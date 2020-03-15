8f6068d39b7df1c03f0e85548e54dac517ef58a9
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-03-15 09:30:55 -0400

    Major Updates

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const form = document.querySelector("#add-show-form");
 function renderShow(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
+  let platform = document.createElement("span");
   let rating = document.createElement("span");
+  let user = document.createElement("span");
   let cross = document.createElement("div");
 
   li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
+  platform.textContent = doc.data().platform;
   rating.textContent = doc.data().rating;
+  user.textContent = doc.data().user;
   cross.textContent = "x";
 
   li.appendChild(name);
+  li.appendChild(platform);
   li.appendChild(rating);
+  li.appendChild(user);
   li.appendChild(cross);
 
   showList.appendChild(li);
@@ -52,8 +58,12 @@ form.addEventListener("submit", e => {
   e.preventDefault();
   firestore.collection("show").add({
     name: form.name.value,
-    rating: form.rating.value
+    platform: form.platform.value,
+    rating: form.rating.value,
+    user: form.user.value
   });
   form.name.value = "";
+  form.platform.value = "";
   form.rating.value = "";
+  form.user.value = "";
 });
